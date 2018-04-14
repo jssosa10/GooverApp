6d8d65885dcabc6f7389dde412540155713a6783
jssosa10 <js.sosa10@uniandes.edu.co>
2018-04-14 18:26:59 -0500

    algo horrible

--- backend/server.py
+++ backend/server.py
@@ -47,7 +47,8 @@ def getCourse():
 	print nombrestemas
 	idsubtemas = [get_subtemas(i) for i in listaidtemas]
 	print idsubtemas
-	nombresubtemas = [get_subtema_nombre(x) for x in idsubtemas]
+	nombresubtemas = [[get_subtema_nombre(i) for i in x] for x in idsubtemas]
+	print nombrestemas
 	idrecursos = [get_recursos_subtema(i) for i in idsubtemas]
 	nombrerecursossub = [[get_recurso_nombre(i) for i in x] for x in idrecursos]
 	print nombrerecursossub
