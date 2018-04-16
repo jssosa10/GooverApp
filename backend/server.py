7ee9a411362df310bbe40cfba5c7a440a28daced
jssosa10 <js.sosa10@uniandes.edu.co>
2018-04-15 23:00:24 -0500

    add files to s3

--- backend/server.py
+++ backend/server.py
@@ -161,8 +161,35 @@ def do_regiter():
 	return json.dumps(user), 200
 @app.route ('/recurso',methods=['POST'])
 def upload_recurso():
-	print request.files
-	return json.dumps('holis carlos')
+	for file in request.files:
+		output = upload_file_to_s3(file, app.config["S3_BUCKET"])
+        return json.dumps('holis carlos')
+	return json.dumps('no files')
+def upload_file_to_s3(file, bucket_name, acl="public-read"):
+
+    """
+    Docs: http://boto3.readthedocs.io/en/latest/guide/s3.html
+    """
+
+    try:
+
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            file.filename,
+            ExtraArgs={
+                "ACL": acl
+                "ContentType": file.content_type
+            }
+        )
+
+    except Exception as e:
+        print("Something Happened: ", e)
+        return e
+
+    return "{}{}".format(app.config["S3_LOCATION"], file.filename)
+
+
 
 if __name__ == "__main__":
 
