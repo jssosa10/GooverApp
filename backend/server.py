9af819e08cbbe010c319dfcb6727e47b3153b6f9
jssosa10 <js.sosa10@uniandes.edu.co>
2018-04-16 17:21:05 -0500

    recursos temas

--- backend/server.py
+++ backend/server.py
@@ -54,9 +54,10 @@ def getCourse():
 	nombresubtemas = [[get_subtema_nombre(i) for i in x] for x in idsubtemas]
 	idrestemas = [[get_recursos_tema(i)] for i in listaidtemas]
 	print idrestemas
-	nombresrestemas = [[[get_recurso_nombre(i) for i in x] for x in y] for y in idrestemas]
+	nombresrestemas = [[get_recurso_nombre(i) for i in x] for x in idrestemas]
 	print nombresrestemas
 	idrecursos = [[get_recursos_subtema(i) for i in x] for x in idsubtemas]
+	print idrecursos
 	nombrerecursossub = [[[get_recurso_nombre(i) for i in x] for x in y] for y in idrecursos]
 	res = {'titulo': get_nombre_curso(str(id)),'temas':[{'nombre':nombrestemas[i],'id':listaidtemas[i],'recursos':[{'nombre':nombresrestemas[i][j][0],'calificacion':nombresrestemas[i][j][1],'tipo':nombresrestemas[i][j][2]} for j in range(len(nombresrestemas[i]))],'subtemas':[{'nombre':nombresubtemas[i][j],'id':idsubtemas[i][j],'recursos':[{'nombre':nombrerecursossub[i][j][k][0],'calificacion':nombrerecursossub[i][j][k][1],'tipo':nombrerecursossub[i][j][k][2]} for k in range(len(idrecursos[i][j])) ]} for j in range(len(idsubtemas[i]))]} for i in range(len(listaidtemas))]}
 	return json.dumps(res)
